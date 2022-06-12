e778a7dbbb0ce33f7a3b95d01767030bd59392a6
tasuku43 <task.teckac@gmail.com>
2022-06-12 22:13:15 +0900

    If the pattern to be searched exists in the file, it was excluded from the analysis.

--- src/Analyser/DependencyAnalyzer.php
+++ src/Analyser/DependencyAnalyzer.php
@@ -27,7 +27,11 @@ class DependencyAnalyzer
 
         $dependencyList = [];
 
-        foreach ((new Finder())->in($absolutePath)->name('*.php')->files() as $file) {
+        $finder = new Finder();
+        // If the pattern string is not present in the file, there is no need to parse dependencies
+        $finder->in($absolutePath)->name('*.php')->contains($pattern)->files();
+
+        foreach ($finder as $file) {
             try {
                 $dependency = $this->resolver->resolve(file_get_contents($file->getRealPath()))->filter($pattern);
             } catch (FailedResolveDependencyException $exception) {
